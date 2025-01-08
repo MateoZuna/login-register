fd10eef5514344278bb86033c7eca278f17e76da
mateoalej96 <mateoalej96@gmail.com>
2025-01-08 15:06:01 +0000

    second commit

---
+++ src/app/Views/box-info/box-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-box-info',
+  imports: [],
+  templateUrl: './box-info.component.html',
+  styleUrl: './box-info.component.css'
+})
+export class BoxInfoComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { LoginComponent } from './Views/login/login.component';
 import { RegisterComponent } from './Views/register/register.component';
 import { SelectorComponent } from './Views/selector/selector.component';
 import { InterfaceUserComponent } from './Views/interface-user/interface-user.component';
+import { BoxInfoComponent } from './Views/box-info/box-info.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, RegisterComponent, SelectorComponent, InterfaceUserComponent],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, SelectorComponent, InterfaceUserComponent, BoxInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
